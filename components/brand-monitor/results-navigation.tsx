1cc4db7b3b4939cc837a6ec25add7779d2ea912e
nishit-pixel <nrana4148@gmail.com>
2025-11-21 11:09:20 +0530

    bug fixes

--- components/brand-monitor/results-navigation.tsx
+++ components/brand-monitor/results-navigation.tsx
@@ -80,18 +80,7 @@ export function ResultsNavigation({
         </div>
         <br></br>
         
-        {/* Analyze another website button - at the bottom */}
-        <div className="pt-4 pb-8 border-t border-gray-200">
-          <button
-            onClick={onRestart}
-            className="w-full text-left px-4 py-3 rounded-[10px] text-sm font-medium transition-all duration-200 bg-[#36322F] text-[#fff] hover:bg-[#4a4542] [box-shadow:inset_0px_-2.108433723449707px_0px_0px_#171310,_0px_1.2048193216323853px_6.325301647186279px_0px_rgba(58,_33,_8,_58%)] hover:translate-y-[1px] hover:scale-[0.98] hover:[box-shadow:inset_0px_-1px_0px_0px_#171310,_0px_1px_3px_0px_rgba(58,_33,_8,_40%)] active:translate-y-[2px] active:scale-[0.97] active:[box-shadow:inset_0px_1px_1px_0px_#171310,_0px_1px_2px_0px_rgba(58,_33,_8,_30%)] flex items-center gap-2"
-          >
-            <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-            </svg>
-            Analyze another website
-          </button>
-        </div>
+
 
        
       </div>
